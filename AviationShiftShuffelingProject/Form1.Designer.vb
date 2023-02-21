318ed33261be5d0d5314f41d133bd143e15e052e
bhathi97 <bhathiya.rb@gmail.com>
2023-02-21 09:39:43 +0530

    Add project files.

---
+++ AviationShiftShuffelingProject/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnFirst = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnFirst
+        '
+        Me.btnFirst.Location = New System.Drawing.Point(236, 104)
+        Me.btnFirst.Name = "btnFirst"
+        Me.btnFirst.Size = New System.Drawing.Size(239, 129)
+        Me.btnFirst.TabIndex = 0
+        Me.btnFirst.Text = "first"
+        Me.btnFirst.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.btnFirst.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnFirst)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnFirst As Button
+End Class
---
+++ AviationShiftShuffelingProject/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Security.Cryptography.X509Certificates
+
+Public Class Form1
+    Private Sub btnFirst_Click(sender As Object, e As EventArgs) Handles btnFirst.Click
+
+        Dim myUnique As New Unique() 'call to the class as object'
+        MsgBox(myUnique.MyString) 'call to the parameter'
+
+
+
+    End Sub
+End Class
